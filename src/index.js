2ff9c114a11a3438bbb3120f980d21ab1492f5e5
Sylvia <sylviapap1@gmail.com>
2020-03-23 15:19:50 -0700

    images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-// const BASE_URL = "http://localhost:3000"
-// const USERS_URL =    `${BASE_URL}/users`
-// const GAMES_URL =    `${BASE_URL}/games`
-// const SCORES_URL =    `${BASE_URL}/scores`
+const BASE_URL = "http://localhost:3000"
+const USERS_URL = `${BASE_URL}/users`
+const GAMES_URL = `${BASE_URL}/games`
+const SCORES_URL = `${BASE_URL}/scores`
 
-// fetch(USERS_URL)
-//     .then(resp => resp.json())
-//     .then(users => {
-//         users.forEach(user => {
-//             const body = document.getElementById("user-list")
-//             const userHTML = `<h1>${user.name}</h1>`
-//             body.innerHTML += userHTML
-//         });
+fetch(USERS_URL)
+    .then(resp => resp.json())
+    .then(users => {
+        users.forEach(user => {
+            const body = document.getElementById("user-list")
+            const userHTML = `<h1>${user.name}</h1>`
+            body.innerHTML += userHTML
+        });
         
-//     })
+    })
+
+fetch(GAMES_URL)
+    .then(resp => resp.json())
+    .then(games => {
+        games.forEach(game => {
+            const gameDiv = document.getElementsByClassName("box")[0]
+            const image = document.createElement("img")
+            image.src = game.background_image
+            gameDiv.appendChild(image)
+        });
+        
+    })
 
 // document.addEventListener("DOMContentLoaded", () => {
     
