3c7f32431009c711fd3594c511803ca4da05240a
Sylvia <sylviapap1@gmail.com>
2020-03-23 14:00:35 -0700

    html & css from wam example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-const BASE_URL = "http://localhost:3000"
-const USERS_URL =    `${BASE_URL}/users`
-const GAMES_URL =    `${BASE_URL}/games`
-const SCORES_URL =    `${BASE_URL}/scores`
+// const BASE_URL = "http://localhost:3000"
+// const USERS_URL =    `${BASE_URL}/users`
+// const GAMES_URL =    `${BASE_URL}/games`
+// const SCORES_URL =    `${BASE_URL}/scores`
 
-fetch(USERS_URL)
-    .then(resp => resp.json())
-    .then(users => {
-        users.forEach(user => {
-            const body = document.getElementById("user-list")
-            const userHTML = `<h1>${user.name}</h1>`
-            body.innerHTML += userHTML
-        });
+// fetch(USERS_URL)
+//     .then(resp => resp.json())
+//     .then(users => {
+//         users.forEach(user => {
+//             const body = document.getElementById("user-list")
+//             const userHTML = `<h1>${user.name}</h1>`
+//             body.innerHTML += userHTML
+//         });
         
-    })
+//     })
+
+// document.addEventListener("DOMContentLoaded", () => {
+    
+// })
