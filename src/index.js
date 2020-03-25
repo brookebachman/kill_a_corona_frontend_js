3ed8e452f19b886d0d87f88e7a2d4961dc4fd2e1
Sylvia <sylviapap1@gmail.com>
2020-03-24 21:24:28 -0700

    post user

--- src/index.js
+++ src/index.js
@@ -3,35 +3,55 @@ const USERS_URL = `${BASE_URL}/users`
 const GAMES_URL = `${BASE_URL}/games`
 const SCORES_URL = `${BASE_URL}/scores`
 
+const loginButton = document.getElementById("login")
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    const startButton = document.getElementById("btn-start")
-    startButton.addEventListener("click", () => {
-       
-       
-    })
-    
+    console.log("dom loaded")
 })
 
-fetch(USERS_URL)
-    .then(resp => resp.json())
-    .then(users => {
-        users.forEach(user => {
-            const body = document.getElementById("user-list")
-            const userHTML = `<h1>${user.name}</h1>`
-            body.innerHTML += userHTML
-        });
-        
-    })
+loginButton.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let userNameInput = e.target.name.value;
+    findOrCreateUser(userNameInput);
+    e.target.reset();
+})
 
-function fetchGame() {
-    fetch(GAMES_URL)
-    .then(resp => resp.json())
-    .then(games => {
-        games.forEach(game => {
-            console.log(game.main_image)
-        });
-        
-    })
-   
+function findOrCreateUser(userNameInput) {
+    fetch(USERS_URL, {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json',
+          Accepts: 'application/json'
+        },
+        body: JSON.stringify({name: userNameInput})
+      })
+        .then(resp => resp.json())
+        .then(userName => displayName(userName))
 }
+
+function displayName(userName) {
+    
+}
+// fetch(USERS_URL)
+//     .then(resp => resp.json())
+//     .then(users => {
+//         users.forEach(user => {
+//             const body = document.getElementById("user-list")
+//             const userHTML = `<h1>${user.name}</h1>`
+//             body.innerHTML += userHTML
+//         });
+        
+//     })
+
+// function fetchGame() {
+//     fetch(GAMES_URL)
+//     .then(resp => resp.json())
+//     .then(games => {
+//         games.forEach(game => {
+//             console.log(game.main_image)
+//         });
+        
+//     })
+   
+// }
