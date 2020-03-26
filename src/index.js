1d4e067be4e26d0b12236cdc6d94110254962dfc
Sylvia <sylviapap1@gmail.com>
2020-03-26 09:52:19 -0700

    removed comments

--- src/index.js
+++ src/index.js
@@ -19,26 +19,3 @@ startButton.style.visibility = "hidden";
 pauseButton.style.visibility = "hidden";
 endButton.style.visibility = "hidden";
 newButton.style.visibility = "hidden";
-
-// fetch(USERS_URL)
-//     .then(resp => resp.json())
-//     .then(users => {
-//         users.forEach(user => {
-//             const body = document.getElementById("user-list")
-//             const userHTML = `<h1>${user.name}</h1>`
-//             body.innerHTML += userHTML
-//         });
-        
-//     })
-
-// function fetchGame() {
-//     fetch(GAMES_URL)
-//     .then(resp => resp.json())
-//     .then(games => {
-//         games.forEach(game => {
-//             console.log(game.main_image)
-//         });
-        
-//     })
-   
-// }
