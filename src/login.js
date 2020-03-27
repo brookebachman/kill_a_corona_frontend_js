93a496f8695bb66fc95854bdd33cb8a8f78e9ff2
Sylvia <sylviapap1@gmail.com>
2020-03-26 21:40:38 -0700

    login/welcome screen fix

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 // Login Elements
 const loginForm = document.getElementById("login")
-const currentUserDiv = document.getElementById("current-user")
-const main = document.querySelector("main")
+const welcomeDiv = document.getElementById("welcome")
+const greeting = document.getElementById("welcome-user")
 
 login.addEventListener("submit", (e) => {
   login.style.visibility = "hidden";
@@ -23,30 +23,12 @@ function findOrCreateUser(userNameInput) {
         .then(resp => resp.json())
         .then(function(user){
             displayName(user);
-            const name = document.getElementById("current-user")
-            name.setAttribute('data-id' , user.id);
+            greeting.setAttribute('data-id' , user.id);
         })
   }    
 
 function displayName(user) {
-  // currentUserDiv.innerHTML = `<p data-id= ${user.id}id="display-user">Welcome, ${user.name}!</p>`;
-  let p = document.createElement("p")
-  p.id = "greet-user"
   let userName = user.name
-  p.innerText = `Welcome, ${userName}!`
-  currentUserDiv.appendChild(p);
-  delayShow();
-}
-
-// Weird delay on transition from login to start button
-// Hacky solution, come back to this later 
-
-let startDelay;
-
-function delayShow() {
-  startDelay = setTimeout(showStart, 1000);
-}
-
-function showStart() {
-  currentUserDiv.appendChild(startButton)
+  greeting.innerText = `Welcome, ${userName}!`
+  welcomeDiv.appendChild(startButton);
 }
